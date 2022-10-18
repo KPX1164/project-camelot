1349b1121905065e2feae8c03104c86b736484d7
College of Arts <CAMT@Copters-MacBook-Pro.local>
2022-10-18 13:02:30 +0700

    HomeScreen Updated👏

--- src/main/java/se233/camelot/controller/MainViewController.java
+++ src/main/java/se233/camelot/controller/MainViewController.java
@@ -16,55 +16,16 @@ import java.io.IOException;
 public class MainViewController {
 
     @FXML
-    private Button startBtn ;
-    @FXML
-    private Button quitBtn;
-    @FXML
-    private ImageView overLayerH;
-    @FXML
-    private ImageView askLayer;
-    @FXML
-    private ImageView yesView;
-    @FXML
-    private Button yesBtn;
-    @FXML
-    private ImageView noView;
-    @FXML
-    private Button noBtn;
-    @FXML
-    private ImageView parinyaLayer;
-
+    private Button tapBtn ;
 
 
 
     @FXML
     public void initialize()  {
 
-        startBtn.setOnAction( e -> {
+        tapBtn.setOnAction( e -> {
             SceneController.navigateTo("MenuView");
         });
-        
-        quitBtn.setOnAction(event -> {
-            overLayerH.setVisible(true);
-            askLayer.setVisible(true);
-            parinyaLayer.setVisible(true);
-            yesView.setVisible(true);
-            yesBtn.setVisible(true);
-            noView.setVisible(true);
-            noBtn.setVisible(true);
 
-            yesBtn.setOnAction(yesEvent ->{
-                System.exit(0);
-            });
-            noBtn.setOnAction(noEvent ->{
-                overLayerH.setVisible(false);
-                askLayer.setVisible(false);
-                parinyaLayer.setVisible(false);
-                yesView.setVisible(false);
-                yesBtn.setVisible(false);
-                noView.setVisible(false);
-                noBtn.setVisible(false);
-            });
-        });
     }
 }
--- src/main/java/se233/camelot/controller/MenuViewController.java
+++ src/main/java/se233/camelot/controller/MenuViewController.java
@@ -2,6 +2,7 @@ package se233.camelot.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 public class MenuViewController {
 
@@ -9,8 +10,24 @@ public class MenuViewController {
     private Button playBtn ;
     @FXML
     private Button settingBtn;
+
     @FXML
-    private Button creditBtn;
+    private Button quitBtn;
+    @FXML
+    private ImageView overLayerH;
+    @FXML
+    private ImageView askLayer;
+    @FXML
+    private ImageView yesView;
+    @FXML
+    private Button yesBtn;
+    @FXML
+    private ImageView noView;
+    @FXML
+    private Button noBtn;
+    @FXML
+    private ImageView parinyaLayer;
+
 
 
 
@@ -25,8 +42,27 @@ public class MenuViewController {
             SceneController.navigateTo("SettingView");
         });
 
-        creditBtn.setOnAction(event -> {
-            SceneController.navigateTo("CreditView");
+        quitBtn.setOnAction(event -> {
+            overLayerH.setVisible(true);
+            askLayer.setVisible(true);
+            parinyaLayer.setVisible(true);
+            yesView.setVisible(true);
+            yesBtn.setVisible(true);
+            noView.setVisible(true);
+            noBtn.setVisible(true);
+
+            yesBtn.setOnAction(yesEvent ->{
+                System.exit(0);
+            });
+            noBtn.setOnAction(noEvent ->{
+                overLayerH.setVisible(false);
+                askLayer.setVisible(false);
+                parinyaLayer.setVisible(false);
+                yesView.setVisible(false);
+                yesBtn.setVisible(false);
+                noView.setVisible(false);
+                noBtn.setVisible(false);
+            });
         });
     }
 }
